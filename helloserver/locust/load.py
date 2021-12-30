7e8c88d32e6d1c01e2e415adb551b826395a88a1
keptn <keptn@keptn.com>
2021-12-30 07:33:01 +0000

    Added resources

--- helloserver/locust/load.py
+++ helloserver/locust/load.py
@@ -7,15 +7,3 @@ class WebsiteUser(HttpUser):
     @task
     def index(self):
         self.client.get("/")
-
-    @task
-    def get_cart(self):
-        self.client.get("/carts/1")
-
-    @task
-    def get_cart_items(self):
-        self.client.get("/carts/1/items")
-
-    @task
-    def health(self):
-        self.client.get("/health")
